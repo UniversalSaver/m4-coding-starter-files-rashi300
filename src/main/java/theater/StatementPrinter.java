b52d5243a829936fa9c438b9fb414f8ad4f9d114
Nabhan Rashid <nabhanbd06@gmail.com>
2025-11-20 01:11:59 -0500

    I'm sorry if anyone sees this, I think I got confused by an old version of this assignment.
    Fixed the StatementPrinter output. Fixed the log.txt to include 2.4. And it seems the old version had different naming conventions for the functions (I can't even find volumeCreditsFor in the new README.md)

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -8,10 +8,12 @@ import java.util.Map;
  * This class generates a statement for a given invoice of performances.
  */
 public class StatementPrinter {
-    private final StatementData statementData;
+    private final Invoice invoice;
+    private final Map<String, Play> plays;
 
     public StatementPrinter(Invoice invoice, Map<String, Play> plays) {
-        statementData = new StatementData(invoice, plays);
+        this.invoice = invoice;
+        this.plays = plays;
     }
 
     /**
@@ -21,27 +23,101 @@ public class StatementPrinter {
      */
     public String statement() {
 
-        return renderPlainText();
-    }
-
-    private String renderPlainText() {
-        final StringBuilder result = new StringBuilder("Statement for " + statementData.getCustomer() + "\n");
-        for (PerformanceData performanceData : statementData.getPerformances()) {
+        final StringBuilder result = new StringBuilder("Statement for " + getInvoice().getCustomer() + "\n");
+        for (Performance performance : getInvoice().getPerformances()) {
             result.append(String.format("  %s: %s (%s seats)%n",
-                    performanceData.getName(), usd(performanceData.getAmount()),
-                    performanceData.getAudience()));
+                    getPlay(performance).getName(), usd(getAmount(performance)),
+                    performance.getAudience()));
         }
 
-        result.append(String.format("Amount owed is %s%n", usd(statementData.totalAmount())));
-        result.append(String.format("You earned %s credits%n", statementData.volumeCredits()));
+        result.append(String.format("Amount owed is %s%n", usd(getTotalAmount())));
+        result.append(String.format("You earned %s credits%n", getTotalVolumeCredits()));
         return result.toString();
     }
 
-    static String usd(int totalAmount) {
+    private int getTotalAmount() {
+        int result = 0;
+        for (Performance performance : getInvoice().getPerformances()) {
+            // print line for this order
+            result += getAmount(performance);
+        }
+        return result;
+    }
+
+    private int getTotalVolumeCredits() {
+        int result = 0;
+        for (Performance performance : getInvoice().getPerformances()) {
+
+            // add volume credits
+            result += getVolumeCredits(performance);
+
+        }
+        return result;
+    }
+
+    private static String usd(int totalAmount) {
         return NumberFormat.getCurrencyInstance(Locale.US).format(totalAmount / Constants.PERCENT_FACTOR);
     }
 
-    StatementData getStatementData() {
-        return statementData;
+    private int getVolumeCredits(Performance performance) {
+        int result = 0;
+        result += Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
+        // add extra credit for every five comedy attendees
+        if ("comedy".equals(getPlay(performance).getType())) {
+            result += performance.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+        }
+        return result;
+    }
+
+    private Play getPlay(Performance performance) {
+        return getPlays().get(performance.getPlayID());
+    }
+
+    private int getAmount(Performance performance) {
+        return amountFor(performance);
+    }
+
+    private int amountFor(Performance performance) {
+        int result;
+
+        switch (getPlay(performance).getType()) {
+            case "tragedy":
+                result = Constants.TRAGEDY_BASE_AMOUNT;
+                if (performance.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
+                    result += Constants.TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON
+                            * (performance.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
+                }
+                break;
+            case "comedy":
+                result = Constants.COMEDY_BASE_AMOUNT;
+                if (performance.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
+                    result += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
+                            + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
+                            * (performance.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
+                }
+                result += Constants.COMEDY_AMOUNT_PER_AUDIENCE * performance.getAudience();
+                break;
+            default:
+                throw new UnexpectedPlayTypeException(
+                        String.format("unknown type: %s", getPlay(performance).getType()));
+        }
+        return result;
+    }
+
+    public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public Map<String, Play> getPlays() {
+        return plays;
+    }
+
+    /**
+     * An exception thrown if a play type is not defined.
+     */
+    static class UnexpectedPlayTypeException extends RuntimeException {
+        UnexpectedPlayTypeException(String message) {
+            super(message);
+        }
     }
 }
